d63dfe8543fb033a89a58aba6dbdf7ab89fcb696
fabio.morais <morais.fabiio@gmail.com>
2023-10-20 12:12:44 -0300

    feat: initial config

---
+++ managevotes/src/test/java/com/challenger/managevotes/ManagevotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenger.managevotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagevotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
