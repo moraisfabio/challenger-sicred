db2c816c86d363535ff83bcf3164961a92c6869c
fabio.morais <morais.fabiio@gmail.com>
2023-10-24 17:58:38 -0300

    feat: implementing system of message

---
+++ managevotes/src/main/java/com/challenger/managevotes/core/connections/RabbitMQConnection.java
@@ -0,0 +1,52 @@
+package com.challenger.managevotes.core.connections;
+
+import com.challenger.managevotes.core.constants.RabbitmqConstans;
+import jakarta.annotation.PostConstruct;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.PersistenceUnit;
+import org.springframework.amqp.core.AmqpAdmin;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RabbitMQConnection {
+    private static final String NAME_EXCHANGE = "amq.direct";
+    @Autowired
+    private AmqpAdmin amqpAdmin;
+
+    public RabbitMQConnection(AmqpAdmin amqpAdmin){
+        this.amqpAdmin = amqpAdmin;
+    }
+
+
+    private Queue rowQueue(String rowName) {return new Queue(rowName, true, false, false);}
+
+    private DirectExchange tradeDirect(){
+        return new DirectExchange(NAME_EXCHANGE);
+    }
+
+    private Binding relation(Queue row, DirectExchange trade){
+        return new Binding(row.getName(), Binding.DestinationType.QUEUE, trade.getName(), row.getName(), null);
+    }
+
+    @PostConstruct
+    private void add(){
+       Queue rowVote = this.rowQueue(RabbitmqConstans.ROW_VOTE);
+
+       DirectExchange trade = this.tradeDirect();
+
+       Binding callRowVote = this.relation(rowVote, trade);
+
+       //creating row in RabbitMQ
+       this.amqpAdmin.declareQueue(rowVote);
+
+       this.amqpAdmin.declareExchange(trade);
+
+       this.amqpAdmin.declareBinding(callRowVote);
+    }
+
+}
---
+++ managevotes/src/main/java/com/challenger/managevotes/core/constants/RabbitmqConstans.java
@@ -0,0 +1,5 @@
+package com.challenger.managevotes.core.constants;
+
+public class RabbitmqConstans {
+    public static final String ROW_VOTE = "VOTE";
+}
