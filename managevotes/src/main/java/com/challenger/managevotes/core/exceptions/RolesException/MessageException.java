a68d72cb01b271756d670c82af4185bf949e14ba
fabio.morais <morais.fabiio@gmail.com>
2023-10-24 17:58:56 -0300

    feat: implementing exceptions message

---
+++ managevotes/src/main/java/com/challenger/managevotes/core/exceptions/RolesException/MessageException.java
@@ -0,0 +1,42 @@
+package com.challenger.managevotes.core.exceptions.RolesException;
+
+public enum MessageException {
+    CLOSED_SESSION{
+        @Override
+        String getMessageError(){
+            return "This session was closed!";
+        }
+    },
+    SESSION_NOT_FOUND{
+        @Override
+        String getMessageError(){
+            return "Session not found!";
+        }
+    },
+    SESSION_ALREADY_EXISTS{
+        @Override
+        String getMessageError(){
+            return "Session already exists with the identifier!";
+        }
+    },
+    SCHEDULE_NOT_FOUND{
+        @Override
+        String getMessageError(){
+            return "Schedule not found!";
+        }
+    },
+    REGISTERED_VOTE{
+        @Override
+        String getMessageError(){
+            return "You already voted!";
+        }
+    },
+    USER_NOT_FOUND{
+        @Override
+        String getMessageError(){
+            return "User not found!";
+        }
+    };
+
+    abstract String getMessageError();
+}
---
+++ managevotes/src/main/java/com/challenger/managevotes/core/exceptions/RolesException/RolesException.java
@@ -0,0 +1,16 @@
+package com.challenger.managevotes.core.exceptions.RolesException;
+
+import org.springframework.http.HttpStatus;
+
+public class RolesException extends RuntimeException{
+    private final HttpStatus httpStatus;
+
+    public RolesException(MessageException messageException, HttpStatus httpStatus) {
+        super(messageException.getMessageError(), null, true, false);
+        this.httpStatus = httpStatus;
+    }
+
+    public int getHttpStaus(){
+        return this.httpStatus.value();
+    }
+}
